42586a657ae64df2c891f7e11211133ee9b50b3b
kai <kwen2921@gmail.com>
2023-10-29 17:08:47 -0400

    adds UploadPage

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import imageUpload from "../../assets/images/Images/Upload-video-preview.jpg";
+import "./UploadPage.scss";
+
 const UploadPage = () => {
     return (
-        <div>
-            This is UploadPage!
+        <div className='upload'>
+            <hr className='upload__divider'></hr>
+            <h1 className='upload__title'>Upload Video</h1>
+            <hr className='upload__divider-tablet'></hr>
+            <form className='upload__form'>
+                <section className='upload__section'>
+                    <div className='upload__thumbnail'>
+                        <h3 className='upload__heading'>VIDEO THUMBNAIL</h3>
+                        <img className='upload__image' src={imageUpload} alt="" />
+                    </div>
+                    <div className='upload__input'>
+                        <label className='upload__description-title' htmlFor='uploadText'>TITLE YOUR VIDEO</label><br />
+                        <input className='upload__input-title' placeholder='Add a title to your video'></input><br />
+                        <label className='upload__description'>ADD A VIDEO DESCRIPTION</label><br />
+                        <textarea className='upload__input-description' placeholder='Add a description to your video'></textarea>
+                    </div>
+                </section>
+                <hr className='upload__divider-tablet-below'/>
+                <div className='upload__buttons'>
+                    <button className='upload__publish'>PUBLISH</button>
+                    <button className='upload__cancel'>CANCEL</button>
+                </div>
+            </form>
+
+
+
         </div>
     );
 };
